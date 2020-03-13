796795a9d3b6b1328891e1ce00a794386fa5e0e9
LeoSanchez89 <58442719+LeoSanchez89@users.noreply.github.com>
2020-03-13 14:15:39 -0400

    actions complete

--- data/api/actionRouter.js
+++ data/api/actionRouter.js
@@ -1,23 +1,26 @@
 const express = require("express");
 const router = express.Router();
 const Actions = require("../helpers/actionModel.js");
+// const Projects = require("../helpers/projectModel.js");
 
+// get all actions
 router.get("/", async (req, res) => {
 	try {
 		const actions = await Actions.get(req.params.id);
 		res.status(200).json(actions);
 	} catch (err) {
-		res.status(500).json({ message: "error fetching actions", error });
+		res.status(500).json({ message: "error fetching actions", err });
 	}
 });
 
-router.post("/", async (req, res) => {
-    try {
-        const post = await Actions.insert(req.body);
-        res.status(201).json(post);
-    } catch (err) {
-        res.status(500).json({ message: "error posting action", error });
-    }
-})
+// get action by id
+router.get("/:id", async (req, res) => {
+	try {
+		const actions = await Actions.get(req.params.id);
+		res.status(200).json(actions);
+	} catch (err) {
+		res.status(500).json({ message: "error fetching actions", err });
+	}
+});
 
 module.exports = router;
--- data/api/projectRouter.js
+++ data/api/projectRouter.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const Projects = require("../helpers/projectModel");
+const Actions = require("../helpers/actionModel.js");
 
 // get all
 router.get("/", async (req, res) => {
@@ -24,23 +25,23 @@ router.get("/:id", async (req, res) => {
 
 // post new
 router.post("/", async (req, res) => {
-    try {
-        const post = await Projects.insert(req.body);
-        res.status(201).json(post);
-    } catch (err) {
-        res.status(500).json({ message: "error posting project", err });
-    }
+	try {
+		const post = await Projects.insert(req.body);
+		res.status(201).json(post);
+	} catch (err) {
+		res.status(500).json({ message: "error posting project", err });
+	}
 });
 
 // update
 router.put("/:id", async (req, res) => {
-    try {
-        const revise = await Projects.update(req.params.id, req.body);
-        res.status(200).json(revise);
-    } catch (err) {
-        res.status(500).json({ message: "error updating project", err });
-    }
-})
+	try {
+		const revise = await Projects.update(req.params.id, req.body);
+		res.status(200).json(revise);
+	} catch (err) {
+		res.status(500).json({ message: "error updating project", err });
+	}
+});
 
 // delete
 router.delete("/:id", async (req, res) => {
@@ -63,5 +64,43 @@ router.get("/:id/actions", async (req, res) => {
 		res.status(500).json({ message: "error fetching project actions", err });
 	}
 });
+
+// post action to project
+router.post("/:id/actions", async (req, res) => {
+	const actionData = req.body;
+	try {
+		actionData.project_id = req.params.id;
+		const action = await Actions.insert(actionData);
+		res.status(201).json(action);
+	} catch (err) {
+		res.status(500).json({ message: "error posting project actions", err });
+	}
+});
+
+// edit action on project
+router.put("/:id/actions/:actid", async (req, res) => {
+	const actionData = req.body;
+	try {
+        actionData.project_id = req.params.id;        
+		const action = await Actions.update(req.params.actid, actionData);
+		res.status(201).json(action);
+	} catch (err) {
+		res.status(500).json({ message: "error updating project actions", err });
+	}
+});
+
+// delete action on project
+router.delete("/:id/actions/:actid", async (req, res) => {
+	const actionData = req.body;
+	try {
+        actionData.project_id = req.params.id;
+        const action = await Actions.get(req.params.actid);
+		const del = await Actions.remove(req.params.actid);
+		res.status(201).json({message: `action: ${action.description} has been removed`});
+	} catch (err) {
+		res.status(500).json({ message: "error deleting project actions", err });
+	}
+});
+
 
 module.exports = router;
